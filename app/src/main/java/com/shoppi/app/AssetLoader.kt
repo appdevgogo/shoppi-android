9753361d62ae1258e0b8d0694414f6b61194cc76
Kyle Yoo <yeongjin.yoo.dev@gmail.com>
2022-12-08 00:19:54 +0900

    Section 7: 쇼핑앱 UI구성(Home화면)

---
+++ app/src/main/java/com/shoppi/app/AssetLoader.kt
@@ -0,0 +1,31 @@
+package com.shoppi.app
+
+import android.content.Context
+import android.util.Log
+
+class AssetLoader {
+
+    fun getJsonString(context: Context, fileName: String): String? {
+        return kotlin.runCatching {
+            loadAsset(context, fileName)
+        }.getOrNull()
+
+    }
+
+    private fun loadAsset(context: Context, fileName: String): String {
+
+        return context.assets.open(fileName).use { inputStream ->
+            val size = inputStream.available()
+            val byte = ByteArray(size)
+            inputStream.read(byte)
+            String(byte)
+
+
+        }
+
+
+    }
+
+
+
+}
